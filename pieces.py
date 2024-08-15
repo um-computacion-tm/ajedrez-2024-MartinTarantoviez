d182bf40e7612a1e375808243f12ba050161a5be
Martin-Tarantoviez <m.tarantoviez@alumno.um.edu.ar>
2024-08-14 23:57:49 -0300

    piezas

--- pieces.py
+++ pieces.py
@@ -1,9 +1,15 @@
 class Piece:
-    def __init__(self,color):
-        self.__color__ = color
- 
+    def __init__(self, color):
+        self.__color__ = color  
+
+    @property
+    def color(self):
+        return self.__color__
+
 class Rook(Piece):
-    ...
-    
+    def __init__(self, color):
+        super().__init__(color)
+
 class Pawn(Piece):
-    ...
+    def __init__(self, color):
+        super().__init__(color)
